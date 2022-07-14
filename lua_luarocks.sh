b2d4b3eb0f71f11a1c10800d2dadb6f576df4f46
TECHOPC <34199654+TECHOPC@users.noreply.github.com>
2022-07-14 17:37:08 -0300

    Update lua_luarocks.sh

--- lua_luarocks.sh
+++ lua_luarocks.sh
@@ -1,3 +1,20 @@
+#!/bin/bash
+
+
+#############  SCRIPT DE INTALAÇÃO LUA 5.3 E LUAROCKS 3.8.0  ############
+# 									                                    
+# lua_luarocks.sh -            			            
+#									                                    
+# Autor: TECHOPC               	
+# Data Criação: 14/07/2022				                        		
+#									                                                                                                   										 
+# Exemplo de uso (dar permissão primeiro pro script e depois executar)  
+#      sudo chmod +x lua_luarocks.sh	         		
+#      sudo ./lua_luarocks.sh	         		
+#							                                    
+#########################################################################
+
+
 cd home
 sudo apt install zip -y
 sudo apt install unzip -y
@@ -9,12 +26,14 @@ sudo tar -xvf lua-5.3.5.tar.gz
 cd lua-5.3.5
 sudo make linux test
 sudo make install
+cd ..
 sudo wget https://luarocks.org/releases/luarocks-3.8.0.tar.gz
 sudo tar -xvf luarocks-3.8.0.tar.gz
 cd luarocks-3.8.0
 ./configure --with-lua-include=/usr/local/include
 sudo make
 sudo make install
+cd ..
 sudo rm -r lua-5.3.5
 sudo rm -r luarocks-3.8.0
 sudo rm -f luarocks-3.8.0.tar.gz
@@ -22,3 +41,5 @@ sudo rm -f lua-5.3.5.tar.gz
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt autoclean 
 sudo apt autoremove -y
+
+echo -e "\033[01;32;40mTHE END!\033[0m"
